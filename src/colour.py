fd99dcb8264e8b02e2a720025dc148ea5330ed97
Isaak <isaak.buslovich@gmail.com>
2023-01-09 13:42:40 +0100

    Update colour.py
    Added new --web-safe mode.

--- src/colour.py
+++ src/colour.py
@@ -8,6 +8,22 @@ def hex_to_rgb(hex_color):
 def rgb_to_hex(rgb_color):
     return '#{:02x}{:02x}{:02x}'.format(*(int(c * 255 + 0.5) for c in rgb_color))
 
+def get_nearest_web_safe_color(color):
+    r, g, b = color
+    r = round(r * 255 / 51) * 51
+    g = round(g * 255 / 51) * 51
+    b = round(b * 255 / 51) * 51
+    return (r / 255, g / 255, b / 255)
+
+def convert_web_safe_to_shorthand(color):
+    # Strip the leading '#' character from the color string
+    color = color[1:]
+
+    if color[0] == color[1] and color[2] == color[3] and color[4] == color[5]:
+        return f"#{color[0]}{color[2]}{color[4]}"
+    else:
+        return f"#{color}"
+
 def shift_hue(rgb_color, amount):
     h, l, s = colorsys.rgb_to_hls(*rgb_color)
     h = (h + amount) % 1.0
@@ -31,6 +47,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate color schemes from a given color.')
     parser.add_argument('-c', '--colors', nargs='*', help='one or more colors in hexadecimal code format', action='store', dest='color_str', default='')
     parser.add_argument('-t', '--tui', action='store_true', help='show a text-based user interface to enter colors', default=False)
+    parser.add_argument('-w', '--web-safe', action='store_true', help='use web-safe colors', default=False)
     args = parser.parse_args()
 
     color_str = ' '.join(args.color_str) if args.color_str else input("Enter one or more colors in hexadecimal code format separated by spaces: ")
@@ -42,10 +59,19 @@ def main():
         print("No colors were entered.")
     else:
         avg_color = tuple(sum(c) / num_colors for c in zip(*colors))
-        print(f"The input is: {rgb_to_hex(avg_color)}")
+        color_hex = rgb_to_hex(avg_color)
+        print(f"The input is: {color_hex}")
+        if args.web_safe:
+            web_safe_color = get_nearest_web_safe_color(avg_color)
+            web_safe_color_hex = rgb_to_hex(web_safe_color)
+            shorthand = convert_web_safe_to_shorthand(web_safe_color_hex)
+            if web_safe_color == avg_color:
+                print(f"{shorthand} is a websafe color.")
+            else:
+                print(f"Closest websafe color is: {shorthand}")
 
     # Generate and print color schemes
-    color = avg_color
+    color = avg_color if not args.web_safe else web_safe_color
     print("Complementary Color Scheme:", ' '.join(rgb_to_hex(c) for c in generate_complementary(color)))
     print("Analogous Color Scheme:", ' '.join(rgb_to_hex(c) for c in generate_analogous(color)))
     print(f"Monochromatic Color Scheme: {' '.join(rgb_to_hex(c) for c in generate_monochromatic(color))}")
