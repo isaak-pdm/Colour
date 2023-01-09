b48a4d225b6b37bd5206a277692f1179ae9c6763
Isaak <isaak.buslovich@gmail.com>
2023-01-09 01:40:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(
-    name='Colours',
+setup(name='Colours',
     version='0.1',
     description='A simple tool that aims to help using Colours',
     author='Isaak',
+    license='MIT',
     # author_email='ich@beispiel.de',
     packages=['colours'],
     include_package_data=True,
